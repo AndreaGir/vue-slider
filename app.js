99d7b1ef078be3fb0cf45a161a50c788c9bef3c8
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-24 17:15:43 +0100

    2

---
+++ app.js
@@ -0,0 +1,33 @@
+const app = new Vue(
+    {
+        el: '#carosello',
+        carte: [
+            {
+                nome: 'olanda',
+                img: 'img/01.jpg',
+                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+            },
+            {
+                nome: 'svizzera',
+                img: 'img/012.jpg',
+                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+            },
+            {
+                nome: 'francia',
+                img: 'img/03.jpg',
+                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+            },
+            {
+                nome: 'gambogia',
+                img: 'img/04.jpg',
+                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+            },
+            {
+                nome: 'mexico',
+                img: 'img/05.jpg',
+                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+            }
+        ]
+    
+    
+})
