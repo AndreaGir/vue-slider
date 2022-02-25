6d8c8ad68a698d86ab6936825f23451bba5cf1f0
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-25 23:53:32 +0100

    ...

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
 const app = new Vue(
     {
         el: '#carosello',
-        carte: [
-            {
-                nome: 'olanda',
-                img: 'img/01.jpg',
-                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+        data: {
+            carte: [
+                {
+                    nome: 'Olanda',
+                    img: 'img/01.jpg',
+                    descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+                },
+                {
+                    nome: 'Svizzera',
+                    img: 'img/02.jpg',
+                    descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+                },
+                {
+                    nome: 'Francia',
+                    img: 'img/03.jpg',
+                    descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+                },
+                {
+                    nome: 'Gambogia',
+                    img: 'img/04.jpg',
+                    descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+                },
+                {
+                    nome: 'Mexico',
+                    img: 'img/05.jpg',
+                    descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+                }
+            ],
+
+            currentIndex: 0
+
+        },
+        methods: {
+            avanti: function(){
+                /* if( this.currentIndex  < this.carte.lenght - 1 ) */
+                this.currentIndex++
+                /* else
+                    this.currentIndex = 0 */
             },
-            {
-                nome: 'svizzera',
-                img: 'img/012.jpg',
-                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
-            },
-            {
-                nome: 'francia',
-                img: 'img/03.jpg',
-                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
-            },
-            {
-                nome: 'gambogia',
-                img: 'img/04.jpg',
-                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
-            },
-            {
-                nome: 'mexico',
-                img: 'img/05.jpg',
-                descrizione: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, totam!',
+            indietro: function(){
+                /* if(this.currentIndex > 0 ) */
+                this.currentIndex--
+                /* else
+                    this.currentIndex = this.carte.lenght -1 */
             }
-        ]
+
+
+        }
     
     
 })
